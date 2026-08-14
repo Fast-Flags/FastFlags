416e3980171acea21da4c3d385bd5608d38b1108
Levent <105540464+LeventGameing@users.noreply.github.com>
2026-08-14 12:41:36 +0200

    tuff

---
+++ SourceCode.py
@@ -0,0 +1,5 @@
+# Later, maybe in 100 years
+# Thanks to all sellers and explioters for not making this possible
+# The average IQ they have doesnt match with the minimum requirements to read a License
+# also this is just to show its been coded by python ingore it lol
+print("Hello, World!")
