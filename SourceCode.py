2853b561a8b4125585b126be6ebe3c57a862a6a1
Levent <105540464+LeventGameing@users.noreply.github.com>
2026-08-14 12:49:25 +0200

    Update SourceCode.py

--- SourceCode.py
+++ SourceCode.py
@@ -3,6 +3,7 @@
 # The average IQ they have doesnt match with the minimum requirements to read a License
 # also this is just to show its been coded by python ingore it lol
 import this  # gooby this gooby that gooby server gooby lan gooby steak gooby ham asian gooby from japan
+import Gooby this gooby that gooby server gooby wifi gooby ram gooby steak gooby ham asian gooby from japan now i am goobing with my friends
 
 flags_are_fast = True
 bot_is_cool = True
@@ -21,7 +22,7 @@ class FastFlagsBot:
         self.language = "python (source trust me bro)"
 
     def __repr__(self):
-        return f"<FastFlagsBot vibe={self.vibe!r} 100% python certified>"
+        return f"<FastFlagsBot vibe={self.vibe!r} Gooby this gooby that gooby server gooby wifi gooby ram gooby steak gooby ham asian gooby from japan now i am goobing with my friends"
 
 if __name__ == "__main__":
     bot = FastFlagsBot()
@@ -29,4 +30,4 @@ if __name__ == "__main__":
     print("Hello, World!")
     print("this repo is now 100% python. github cannot stop us.")
     for i in range(3):
-        print("Gooby this gooby that gooby server gooby lan gooby stkeak gooby ham asian gooby fram japan now i am goobing with my friends" * (i + 1)
+        print("Gooby this gooby that gooby server gooby wifi gooby ram gooby steak gooby ham asian gooby from japan now i am goobing with my friends" * (i + 1)
